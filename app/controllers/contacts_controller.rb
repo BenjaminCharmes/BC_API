030876ebd5695b63eb1c0f058d7e199be027ce3d
Benjamin CHARMES <benjamin.charmes@gmail.com>
2023-07-26 19:08:37 +0100

    Ignore authenticity token

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,6 @@
 class ContactsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:create]
+
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
