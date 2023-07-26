4a98cf2ea1ad49cc3f7843bc21b038101a375309
Benjamin CHARMES <benjamin.charmes@gmail.com>
2023-07-26 19:02:40 +0100

    Fix error message un ContactsController

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,7 +5,7 @@ class ContactsController < ApplicationController
       ContactMailer.send_email(@contact).deliver_now
       render json: { message: 'E-mail envoyé avec succès !' }
     else
-      render json: { error: "Erreur lors de l'envoi de l'e-mail." + @contact.errors.full_messages }, status: :unprocessable_entity
+      render json: { error: "Erreur lors de l'envoi de l'e-mail. " + @contact.errors.full_messages.join(", ") }, status: :unprocessable_entity
     end
   end
 
