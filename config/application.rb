1453ca171a0c50c43e94436a965c23433de71075
Benjamin CHARMES <benjamin.charmes@gmail.com>
2023-07-27 17:56:30 +0100

    Fix Mailer Portfolio

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,13 @@ module BenjaminCharmesApi
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'https://benjamincharmes.github.io'
+        resource '/send-email', headers: :any, methods: [:post]
+      end
+    end
+
   end
 end
