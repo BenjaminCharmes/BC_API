00aa6cef2622744343e4200b67e6b44038716c1f
Benjamin CHARMES <benjamin.charmes@gmail.com>
2023-07-27 17:51:50 +0100

    Fix Mailer Portfolio

--- config/initializers/cors.rb
+++
@@ -1,7 +0,0 @@
-# config/initializers/cors.rb
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins 'https://benjamincharmes.github.io'
-    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
-  end
-end
